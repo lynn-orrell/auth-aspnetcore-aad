24a13eb927aae321fb15a68a4673c28466fd6c29
David Barkol <dbarkol@hotmail.com>
2018-10-15 23:07:10 -0500

    Removed test projects

--- Application/aspnet-core-webapi/Controllers/ValuesController.cs
+++ Application/aspnet-core-webapi/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace aspnet_core_webapi.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Frank", "Rizzo" };
         }
 
         // GET api/values/5
